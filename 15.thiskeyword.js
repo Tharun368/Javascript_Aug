464c060f16bb30efca4b63f10196360204b5ccb5
Tharun <venkatatharun.fullstack@gmail.com>
2025-09-07 08:45:04 +0530

    javascript

---
+++ 15.thiskeyword.js
@@ -0,0 +1,29 @@
+//global context
+
+
+console.log(this);
+
+this.authors = ["kalidasa, vyasa,"];
+this.books = ["mahabharatam, ramayanam"];
+
+this.getbookDetails = function(){
+    return books;
+
+}
+
+console.log(this);
+
+
+var obj = {
+    name : "Rabibdranath Tagore",
+    DateofBirth : "7th May 1861",
+    book : ["Gitanjali","the home and the world","gora"],
+    achievements : ["Nobel prize in 1913","composer of national anthem of India"],
+    getbookDetails : function(){
+        console.log(this);
+
+    }
+
+}
+
+obj.getbookDetails();   
