1151f79d52597fab109b795d936652e972726df5
tharun <venkatatharun.fullstack@gmail.com>
2025-09-09 22:18:15 +0530

    Update 3.scope.js

--- 3.scope.js
+++ 3.scope.js
@@ -1,17 +1,14 @@
 var signalName = "wi-fi";
 //localblock1
 
-function Room1()
-{
+function Room1() {
     let room1FanStatus = "Very Fast";
     console.log("Room1" ,room1FanStatus);
     console.log("Room1 - Global scope" , signalName);
 }
 
 //localblock2
-function Room2()
-{
-   // console.log("Room1" ,room1FanStatus);
+function Room2() {
     let room2FanStatus = "Very Slow";
     console.log("Room2", room2FanStatus);
     console.log("Room2 - Global scope" , signalName);
